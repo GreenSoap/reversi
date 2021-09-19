0aa7c22e36240c72daabe5f17c6e49ad96978aad
William Sjökvist <sjwi19io@student.ju.se>
2021-09-19 16:51:45 +0200

    Implemented minimax w/o MakeMove

--- FSAI/GameLogic.fs
+++ FSAI/GameLogic.fs
@@ -5,33 +5,84 @@ module Rule =
   let Tie = 4uy
 
 module Tile = 
-  let Empty = 0uy 
+  let Empty = 0uy
   let White = 1uy
   let Black = 2uy
 
 module GameLogic =
   let boardLength = 8
-  
-  let isValidMove (board: byte[,], tile: byte, x: int, y: int) = 
+  let error = 99uy
+  let directions = [ 
+    (1, -1)
+    (0, 1)
+    (1, 1)
+    (-1, 0)
+    (1, 0)
+    (-1, -1)
+    (0, -1)
+    (1, -1)
+  ]
+
+  let getOpposingTile (tile: byte) =
+    if (tile <> Tile.White && tile <> Tile.Black) then error
+    elif tile = Tile.White then Tile.Black else Tile.White      
+    
+  let isValidMove (board: byte[,]) (tile: byte) (x: int) (y: int) =
     true
     
-  let getValidMoves (board: byte[,], tile: byte) =
-    Seq.map(fun (x, y) -> isValidMove(board, tile, x ,y)) (Seq.cast board)
+  let getValidMoves (board: byte[,]) (tile: byte) = 
+    Seq.map(fun (x, y) -> isValidMove board tile x y) (Seq.cast board)
 
   // Returns the amount of tiles in the board corresponding to the passed tile
-  let getScore (board: byte[,], tile: byte) =
+  let getScore (board: byte[,]) (tile: byte) =
     Seq.length((Seq.filter(fun cell -> cell = tile) (Seq.cast board)))
   
-  let getWinner (board : byte[,]) = 
-    let blackScore = getScore(board, Tile.Black)
-    let whiteScore = getScore(board, Tile.White)
+  let isBoardFull blackScore whiteScore =
+    (blackScore + whiteScore = (boardLength * boardLength))
 
-    if (blackScore = 0 || whiteScore = 0) || 
-      (blackScore + whiteScore = (boardLength * boardLength)) || 
-      (Seq.length(getValidMoves(board, Tile.White)) + Seq.length(getValidMoves(board, Tile.Black)) = 0) then
+  let getWinner (board: byte[,]) = 
+    let blackScore = getScore board Tile.Black
+    let whiteScore = getScore board Tile.White
+
+    if isBoardFull blackScore whiteScore || blackScore = 0 || whiteScore = 0 ||
+      (Seq.length(getValidMoves board Tile.White) + Seq.length(getValidMoves board Tile.Black) = 0) then
 
       if whiteScore > blackScore then Tile.White
       elif blackScore > whiteScore then Tile.Black
       else Rule.Tie
     else
       Tile.Empty
+
+  let getNonEmptyCornerTileAmount(board: byte[,]) (tile: byte) =
+    (List.filter(fun corner -> corner = tile) [
+      board.[0, 0];
+      board.[0, boardLength-1];
+      board.[boardLength-1, 0];
+      board.[boardLength-1, boardLength-1]
+    ]).Length
+
+  // Minimax evaluation 
+  let getBoardEvaluation(board: byte[,]) =
+    let blackScore = getScore board Tile.Black
+    let whiteScore = getScore board Tile.White
+    let blackMobility = Seq.length(getValidMoves board Tile.Black)
+    let whiteMobility = Seq.length(getValidMoves board Tile.White)
+
+    if blackScore = 0 then
+        -200000
+    elif whiteScore = 0 then
+        200000
+    else
+      if isBoardFull blackScore whiteScore || blackMobility + whiteMobility = 0 then
+        if (blackScore < whiteScore) then
+            -100000 - whiteScore + blackScore
+        elif (blackScore > whiteScore) then
+            100000 + blackScore - whiteScore
+        else
+            0
+      else
+        if blackScore + whiteScore > 55 then
+            blackScore - whiteScore
+        else
+          ((blackScore - whiteScore) + (blackMobility - whiteMobility) * 10) + 
+          (getNonEmptyCornerTileAmount board Tile.Black - getNonEmptyCornerTileAmount board Tile.White) * 100
--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -1,17 +1,41 @@
 ﻿namespace FSAI
 
 module Game =
-  // Minimax algo w/ alpha-beta pruning to disregard inconsequential parts of the tree
-  let rec minimax (depth: int, alpha: int, beta: int, isMaximizingPlayer: bool, board: byte[,], tile: byte) =
-      // If the last leaf has no more children, or the game is over
-      if depth = 0 || (GameLogic.getWinner(board) <> Tile.Empty) then
-        //TODO Eval
-        0.0
+  // Minimax algo with alpha-beta pruning to disregard inconsequential parts of the tree
+  let rec minimax (board: byte[,]) (tile: byte) (depth: int) (alpha: int) (beta: int) (isMaximizingPlayer: bool) =
+      // Return static evaluation if the leaf has no more children, or the game is over.
+      if depth = 0 || GameLogic.getWinner(board) <> Tile.Empty then
+        GameLogic.getBoardEvaluation(board)
       else
-        if isMaximizingPlayer then
-          let maxEval = -infinity
+        let validMoves = GameLogic.getValidMoves board tile
+        let minmaxEval = if isMaximizingPlayer then System.Int32.MinValue else System.Int32.MaxValue
 
-          maxEval
-        else
-          let maxEval = +infinity
-          maxEval
+        if Seq.length validMoves = 0 then
+          minimax board (GameLogic.getOpposingTile tile) (depth) alpha beta (not isMaximizingPlayer)
+        else      
+        
+          let rec getBestScoreFromValidMoves (board: byte[,]) (validMoveAmount: int) (tile: byte) (alpha: int) (beta: int) (isMaximizingPlayer: bool) =            
+            let childBoard = Array2D.copy board
+
+            if isMaximizingPlayer then
+              //makeMove to the new board here
+              let eval = minimax childBoard (GameLogic.getOpposingTile tile) (depth-1) alpha beta false
+              let maxEval = max minmaxEval eval
+              let alpha = max minmaxEval alpha
+
+              if (beta <= alpha) then
+                maxEval
+              else                
+                getBestScoreFromValidMoves board (validMoveAmount-1) tile alpha beta false
+            else
+              //makeMove to the new board here
+              let eval = minimax childBoard (GameLogic.getOpposingTile tile) (depth-1) alpha beta true
+              let minEval = min minmaxEval eval
+              let beta = min minmaxEval beta
+
+              if (beta <= alpha) then
+                minEval
+              else                
+                getBestScoreFromValidMoves board (validMoveAmount-1) tile alpha beta true
+
+          getBestScoreFromValidMoves board (Seq.length validMoves) tile alpha beta isMaximizingPlayer
